2e34ce5f2d95d9ad612a41a84af440bae6212509
Joshua Lawson <32377842+JoshuaLawson8@users.noreply.github.com>
2022-12-05 20:00:41 -0800

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: "db-mysql-sfo2-do-user-12649737-0.b.db.ondigitalocean.com",
     PORT: 25060,
     USER: "doadmin",
-    PASSWORD: "AVNS_gwQVyfbOISGCisQMufp",
+    PASSWORD: "REPLACE_ME",
     DB: "Confidential_Hospital_Data"
   };
   
